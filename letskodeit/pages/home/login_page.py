352a33801fd51090c9cdd0d39bbed812231d5246
Anna Bogatko <anna.bogatko7@gmail.com>
2018-11-20 16:41:06 +0200

    add logger

--- letskodeit/pages/home/login_page.py
+++ letskodeit/pages/home/login_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 from base.selenium_driver import SeleniumDriver
+import utilities.custom_logger as cl
+import logging
 
 class LoginPage(SeleniumDriver):
+
+    log = cl.customLogger(logging.DEBUG)
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -13,33 +17,29 @@ class LoginPage(SeleniumDriver):
     _password_field = "user_password"
     _login_button = "commit"
 
-    def getLoginLink(self):
-        return self.driver.find_element(By.LINK_TEXT, self._login_link)
-
-    def getEmailField(self):
-        return self.driver.find_element(By.ID, self._email_field)
-
-    def getPasswordField(self):
-        return self.driver.find_element(By.ID, self._password_field)
-
-    def getLoginButton(self):
-        return self.driver.find_element(By.ID, self._login_button)
-
     def clickLoginLink(self):
-        self.getLoginLink().click()
+        self.elementClick(self._login_link, locatorType="link")
 
     def enterEmail(self, email):
-        self.getEmailField().send_keys(email)
+        self.sendKeys(email, self._email_field)
 
     def enterPassword(self, password):
-        self.getPasswordField().send_keys(password)
+        self.sendKeys(password, self._password_field)
 
     def clickLoginButton(self):
-        self.getLoginButton().click()
+        self.elementClick(self._login_button, locatorType="name")
 
 
-    def login(self, email, password):
+    def login(self, email="", password=""):
         self.clickLoginLink()
         self.enterEmail(email)
         self.enterPassword(password)
         self.clickLoginButton()
+
+    def verifyLoginSuccessful(self):
+        result = self.isElementPresent("//a[contains(text(),'My Courses')]", locatorType="xpath")
+        return result
+
+    def verifyLoginFailed(self):
+        result = self.isElementPresent("//div[@class='alert alert-danger']", locatorType="xpath")
+        return result
